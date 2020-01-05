4bf319d1f31495c9235db6336cd46e18e882b8ca
Hielke Walinga <hielkewalinga@gmail.com>
2020-01-05 21:00:26 +0100

    miniasm almost done

---
+++ main.py
@@ -0,0 +1,368 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Minimap and miniasm.
+"""
+
+import sys
+from collections import deque
+from functools import partial
+from itertools import groupby, islice, repeat, tee
+from operator import itemgetter
+from typing import Callable, Dict, Iterable, Iterator, List, Set, Tuple
+
+complement = str.maketrans("ACGT", "TGCA")
+
+
+def reverse_complement(s: str) -> str:
+    """
+    Create the reverse complement of sequence
+    """
+    return s.translate(complement)[::-1]
+
+
+def sliding_windowing(s: str, k: int) -> Iterator[str]:
+    """
+    Creates a sliding window of k-mers over a sequence (s).
+
+    Parameters
+    ----------
+    s : str
+        The sequence.
+    k : int
+        The size of the k-mer.
+
+    Returns
+    -------
+    it : Iterator[str]
+        The k-mers yield from this iterator.
+    """
+    if len(s) < k:
+        return iter(())
+    # To create this k-mer iterator, we create k letter iterators which
+    # start all one letter further than the previous ones.
+    # Then, get one letter from all these letter iterators and concatenate them.
+    texts = []
+    for ind, t in enumerate(tee(s, k)):
+        for _ in repeat(None, ind):
+            next(t)
+        texts.append(t)
+    return map("".join, zip(*texts))
+
+
+def hash_seq(s: str) -> int:
+    """
+    Hash a sequence (or k-mer).
+
+    Using Phi(A)=0, Phi(C)=1, Phi(G)=2, and Phi(T)=3
+
+    Calculate with s = a_1 ... a_k:
+    Phi(s) = Phi(a_1) x 4 ^ (k-1) + Phi(a_2) x 4 ^ (k-2) ... + Phi(a_k)
+
+    Parameters
+    ----------
+    s : str
+        s is the sequence (or k-mer) that needs to be hashed
+
+    Returns
+    -------
+    x : int
+        Returns the integer hashed
+    """
+    base_values = {"A": 0, "C": 1, "G": 2, "T": 3}
+    k = len(s)
+    return sum(base_values[b] * 4 ** (k - i) for i, b in enumerate(s))
+
+
+def invertable_hash(x: int, p: int) -> int:
+    """
+    Invertible integer hash function.
+
+    Invert a hash integer.
+
+    Algorithm 2.
+
+    A problem with :func:`hash_seq` is that the uninformative poly-A sequence
+    will get the hash value of 0, which is the minimum. To prevent this,
+    the hash function is altered using this function. Invertibility of the
+    hash function is not a requirement, but prevent hash collisions.
+
+    Parameters
+    ----------
+    x : int
+        The hash integer.
+    p : int
+        The amount of bits of the hash.
+
+    Returns
+    -------
+    x : int
+        Return the new hash integer.
+    """
+    m = 2 ** p - 1
+    x = (~x + (x << 21)) & m
+    x = x ^ (x >> 24)
+    x = (x + (x << 3) + (x << 8)) & m
+    x = x ^ (x >> 14)
+    x = (x + (x << 2) + (x << 4)) & m
+    x = x ^ (x >> 28)
+    x = (x + (x << 31)) & m
+    return x
+
+
+Minimizers = Set[Tuple[int, int, bool]]
+Seq_Hash = Callable[[str], int]
+Minimizer_Sketch = Callable[[str], Minimizers]
+Target_Index = Dict[int, List[Tuple[int, int, bool]]]
+# A list with a tuple with the name, and the lenght of the sequences.
+Seq_Info = List[Tuple[str, int]]
+
+
+def compute_minimizers(s: str, w: int, k: int, hash_func: Seq_Hash) -> Minimizers:
+    """
+    Compute the (w, k)-minimizers.
+
+    A minimizer is the smallest k-mer in a window of w k-mer.
+
+    Algorithm 1.
+
+    Parameters
+    ----------
+    s : str
+        The input sequence. For the size of the sequence it should be
+        |s| => w + k - 1
+    w : int
+        The amount of k-mers to use in a window in which to find the smallest k-mer.
+    k : int
+        The size of the k-mer.
+
+    Returns
+    -------
+    M : Minimizers
+        The minimizers are a set with a tuple with the numbers m, i, r
+        Where m is the hash integer,
+        i is the position
+        r is the strand
+    """
+    kmers = sliding_windowing(s, k)
+    w_window = deque(islice(kmers, w))
+    M: Minimizers = set()
+
+    i = 0  # We use 0-indexing.
+    while True:
+
+        # Find minimum k-mer
+        m = sys.maxsize  # 2 ** 63 - 1 on 64 bit platforms.
+        for j, kmer in enumerate(w_window):
+            u, v = hash_func(kmer), hash_func(reverse_complement(kmer))
+            if u != v:  # If reverse complement the same sequence, do not use.
+                m = min(m, u, v)
+
+        # Add minimizer
+        for j, kmer in enumerate(w_window):
+            u, v = hash_func(kmer), hash_func(reverse_complement(kmer))
+            if u == m or v == m:
+                M.add((m, i + j, v < u))
+
+        # Move w window one k-mer further. (If not more k-mers, we are done.)
+        new_kmer = next(kmers, None)
+        if not new_kmer:
+            break
+        w_window.popleft()
+        w_window.append(new_kmer)
+        i += 1
+
+    return M
+
+
+def get_func_minimizer_sketch(w: int, k: int) -> Minimizer_Sketch:
+    """
+    A function to set all values to the :func:`compute_minimizers` function.
+    """
+    return partial(
+        compute_minimizers,
+        w=w,
+        k=k,
+        hash_func=lambda s: invertable_hash(hash_seq(s), 2 * k),
+    )
+
+
+def index_targets(
+    target_seqs: Iterable[Tuple[str, str]], minimizer_sketch: Minimizer_Sketch
+) -> Tuple[Target_Index, Seq_Info]:
+    """
+    Algorithm 3.
+    """
+    H_list = []
+    seq_info = []
+    for t, (name, seq) in enumerate(target_seqs):
+
+        seq_info.append((name, len(seq)))
+
+        M = minimizer_sketch(s)
+        for h, i, r in M:
+            H_list.append((h, (t, i, r)))
+
+    H_list.sort(key=itemgetter(0))
+
+    # By using the dict.fromkeys function, the growth phase of the dict is streamlined.
+    H_dict = dict.fromkeys(map(itemgetter(0), H_list))
+
+    for h, vals in groupby(H_list, key=itemgetter(0)):
+        H_dict[h] = list(vals)
+
+    return H_dict, seq_info
+
+
+def map_query(
+    query_seq: str,
+    query_name: str,
+    target_seq_info: Seq_Info,
+    H: Target_Index,
+    minimizer_sketch: Minimizer_Sketch,
+    epsilon: int,
+    min_subset: int,
+    min_overlap: int,
+):
+    """
+    Algorithm 4.
+    """
+    M = minimizer_sketch(query_seq)
+    A = []
+
+    # Collect minimizers hit
+    for h, i, r in M:
+        for t, i_t, r_t in H[h]:
+            if r == r_t:
+                A.append((t, 0, i - i_t, i_t))
+            else:
+                A.append((t, 1, i + i_t, i_t))
+
+    # Python automatically sorts by first looking at the first value of the tuple,
+    # than the second, than the third, etc.
+    A.sort()
+
+    b = 0
+    for e in range(len(A)):
+        if (
+            e + 1 == len(A)
+            or A[e + 1][0] != A[e][0]
+            or A[e + 1][1] != A[e][1]
+            or A[e + 1][2] - A[e][2] >= e
+        ):
+            potential_overlap = A[b:e]
+            b = e + 1
+
+            begin, end = maximal_colinear_subset(map(itemgetter(3), potential_overlap))
+
+            if end - begin < min_subset:
+                continue
+
+            res = overlap_hit(potential_overlap[begin:end])
+            if res[10] > min_overlap:
+                res[0] = query_name
+                res[1] = len(query_seq)
+                yield res
+
+
+def map_all_queries(query_seqs):
+    """
+    ...
+    """
+
+
+def overlap_hit(minimizer_hits: List[Tuple[int, int, int, int]]):
+    """
+    Return PAF tuple
+    1	string	Query sequence name
+    2	int	Query sequence length
+    3	int	Query start coordinate (0-based)
+    4	int	Query end coordinate (0-based)
+    5	char	‘+’ if query/target on the same strand; ‘-’ if opposite
+    6	string	Target sequence name
+    7	int	Target sequence length
+    8	int	Target start coordinate on the original strand
+    9	int	Target end coordinate on the original strand
+    10	int	Number of matching bases in the mapping
+    11	int	Number bases, including gaps, in the mapping
+    12	int	Mapping quality (0-255 with 255 for missing)
+    """
+    pass
+
+
+def maximal_colinear_subset(li: Iterable[int]) -> Tuple[int, int]:
+    """
+    Find the longest increasing subsequence.
+
+    Returns
+    -------
+    begin, end : Tuple[int, int]
+        Return the first index and the last index of the longest increasing subsequence.
+    """
+    pass
+
+
+def read_fastx(file: str, format: str) -> Iterator[Tuple[str, str]]:
+    """
+    Currently, this function just assumes for fasta that the sequences and the
+    fasta header are on alternating lines.
+
+    Returns
+    -------
+    it : Iterator[Tuple[str, str]]
+        Returns an iterator that yields tuples with the sequence name and the sequence.
+    """
+    if format == "fastq":
+        group_size = 4
+    elif format == "fasta":
+        group_size = 2
+
+    with open(file) as f:
+        ind_line = enumerate(f)
+        for ind, line in ind_line:
+            if ind % group_size == 0:
+                seq_name = line.strip()
+                _, seq = next(ind_line).strip()
+                yield seq_name, seq
+
+
+def minimap(target_seq_file, query_seq_file, output_file_name):
+    """
+    This is the minimap function
+    """
+    target_seqs = read_fastx(target_seq_file, format="fastq")
+    query_seqs = read_fastx(query_seq_file, format="fastq")
+
+    k = 6
+    w = 6
+    epsilon = 500
+    min_subset = 4
+    min_overlap = 100
+
+    minimizer_sketch = get_func_minimizer_sketch(w, k)
+
+    H, target_seq_info = index_targets(target_seqs, minimizer_sketch)
+
+    with open(output_file_name) as f:
+        for query_name, query_seq in query_seqs:
+            for t in map_query(
+                query_seq,
+                query_name,
+                target_seq_info,
+                H,
+                minimizer_sketch,
+                epsilon,
+                min_subset,
+                min_overlap,
+            ):
+                print(*t, seq="\t", file=f)
+
+
+def miniasm(paf_file):
+    """
+    This is the miniasm function
+    """
+
+
+if __name__ == "__main__":
+    print("hi")
